e760ea139e1ba5be618bb2c836bfe63e13772ea3
linroex <linroex@coder.tw>
2024-04-14 22:26:32 +0800

    app list messages

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { env } from 'process';
+var mysql = require('mysql');
 
 const app = express();
 const port = 3000;
@@ -6,6 +8,33 @@ const port = 3000;
 app.get('/', (req, res) => {
   console.log('Request received' + new Date());
   res.send('Hello, World!');
+});
+
+app.get('/health', (req, res) => {
+  res.send('Server is up and running!');
+});
+
+app.get('/msgs', (req, res) => {
+  var connection = mysql.createConnection({
+    host: env.DB_HOST,
+    user: 'app',
+    password: 'app',
+    database: 'app'
+  });
+
+  connection.connect((err: any) => {
+    if (err) {
+      console.error('Error connecting to database');
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    connection.query('SELECT message from message', (err: any, rows: any, fields: any) => {
+      res.send(rows);
+    });
+
+    connection.end();
+  });
 });
 
 app.listen(port, () => {
